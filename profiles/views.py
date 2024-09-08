f99814cb9add8283b48c2e106200e8997d7795ef
CraigB73 <craigbritton73@gmail.com>
2024-09-08 08:04:38 +0200

    Add test to posts app

--- profiles/views.py
+++ profiles/views.py
@@ -27,16 +27,22 @@ class ProfileDetailView(APIView):
     
     def get(self, request, pk ):
         profile= self.get_object(pk)
-        serializer = ProfileSerializer(profile, context={'request': request})
+        serializer = ProfileSerializer(
+            profile, context={'request': request}
+            )
         return Response(serializer.data)
     
     def put(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data, context={'request': request})
+        serializer = ProfileSerializer(
+            profile, data=request.data, context={'request': request}
+            )
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
     
     def delete(self, pk):
         profile = self.get_object(pk)
