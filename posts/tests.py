b12c1f04a3667c782c5c38dd7d078c097be2787d
CraigB73 <craigbritton73@gmail.com>
2024-09-16 20:37:35 +0200

    last two test for post

--- posts/tests.py
+++ posts/tests.py
@@ -53,3 +53,16 @@ class PostDetailViewTests(APITestCase):
     def test_cant_retrieve_post_using_invalid_id(self):
         response = self.client.get('/posts/999/')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_update_own_post(self):
+        self.client.login(username='adam', password='pass')
+        response = self.client.put('/posts/1/', {'title': 'a new title'})
+        post = Post.objects.filter(pk=1).first()
+        self.assertEqual(post.title, 'a new title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_cant_update_another_users_post(self):
+        self.client.login(username='adam', password='pass')
+        response = self.client.put('/posts/2/', {'title': 'a new title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        
