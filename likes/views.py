9907e4c461b5fcb0977b834e49ce23aa1a6bdbff
CraigB73 <craigbritton73@gmail.com>
2024-09-17 21:06:01 +0200

    add Like serializer urls paths and LikeList and LikeDetail

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
-
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Like
+from .serializer import LikeSerializer
 # Create your views here.
+
+class LikeList(generics.ListCreateAPIView):
+    """
+    List likes or create a like if logged in.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
+  
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a like or delete it by id if you own it.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
+      
+    
