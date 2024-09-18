bea5696188c53fa7a501e3fb2db08e12057410d6
CraigB73 <craigbritton73@gmail.com>
2024-09-18 15:45:15 +0200

    add get_like_id and get_followers_id in serializers

--- posts/serializer.py
+++ posts/serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Post
+from likes.models import Like
 
 class PostSerializer(serializers.ModelSerializer):
     """
@@ -10,6 +11,7 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -27,11 +29,20 @@ class PostSerializer(serializers.ModelSerializer):
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+    
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            likes = Like.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            return likes.id if likes else None
+        return None
 
     class Meta:
         model = Post
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image', 'image_filter'
+            'title', 'content', 'image', 'image_filter', 'like_id'
         ]
--- profiles/serializer.py
+++ profiles/serializer.py
@@ -1,13 +1,25 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')#so it can't be edited
     is_owner = serializers.SerializerMethodField() #readonly gets value with the get_is_owner
+    following_id = serializers.SerializerMethodField()
     
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+    
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            return following.id if following else None
+        return None
+            
     class Meta:
         model= Profile
-        fields = ['id', 'owner', 'updated_at', 'name', 'content', 'image', 'created_at', 'is_owner']
+        fields = ['id', 'owner', 'updated_at', 'name', 'content', 'image', 'created_at', 'is_owner', 'following_id']
