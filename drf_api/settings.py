ac606d5e71e4dddda81102a3388e615024a93eee
CraigB73 <craigbritton73@gmail.com>
2024-09-19 20:12:01 +0200

    fixed issue with Allowed_Hosts

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -48,7 +48,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'drfblogapi-fd830f62e40d.herokuapp.com']
+ALLOWED_HOSTS = ["127.0.0.1",
+    ".herokuapp.com",]
 
 
 # Application definition
